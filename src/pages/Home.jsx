5e1bdb520961658ec5dcf84f7d3a6791bc3e5061
JamesWainaina <jamesgatheru9905@gmail.com>
2025-05-24 16:11:58 +0300

    create all index stylings

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='min-h-screen bg-background text-foreground overflow-x-hidden'>
+
+        {/* Theme Toggle */}
+        
+
+        {/* Background Effects */}
+
+        {/* Navbar*/ }
+
+        {/* Main Content */}
+
+        {/* footer */}
+    </div>
+  )
+}
+
+export default Home
